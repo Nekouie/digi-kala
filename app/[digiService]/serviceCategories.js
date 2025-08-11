5ce7bcba20070f0fe71402fa3ca2be934858ed1e
Hossein Nekouie <hossein373737@gmail.com>
2025-08-11 12:36:51 +0330

    second changes

---
+++ app/[digiService]/serviceCategories.js
@@ -0,0 +1,71 @@
+const ServiceCategories = () => {
+    return (
+        <>
+            <div className="w-full h-[10rem] flex items-center justify-center text-center flex-nowrap">
+                <div className="w-10/12 h-9/12 flex row-auto items-baseline justify-center ">
+                    <div className="w-1/14 flex ml-12 flex-col items-center-safe justify-center">
+                        <div
+                            className="bg-teal-900 rounded-full h-13 w-13 mb-2 text-white flex justify-center items-center">p
+                        </div>
+                        <p className="text-xs/6 w-9/12">طلای دیجیتال بدون کارمزد</p>
+                    </div>
+                    <div className="w-1/14 flex ml-12 flex-col items-center justify-center">
+                        <div
+                            className="bg-fuchsia-800 rounded-full h-13 w-13 mb-2 text-white flex justify-center items-center">p
+                        </div>
+                        <p className="text-xs/6 w-9/12">اشتراک پلاس</p>
+                    </div>
+                    <div className="w-1/14 flex ml-12 flex-col items-center justify-center">
+                        <div
+                            className="bg-orange-600 rounded-full h-13 w-13 mb-2 text-white flex justify-center items-center">p
+                        </div>
+                        <p className="text-xs/6 w-9/12">ابزارآلات تخصصی</p>
+                    </div>
+                    <div className="w-1/14 flex ml-12 flex-col items-center justify-center">
+                        <div
+                            className="bg-cyan-700 rounded-full h-13 w-13 mb-2 text-white flex justify-center items-center">p
+                        </div>
+                        <p className="text-xs/6 w-9/12">موبایل بهترین‌قیمت</p>
+                    </div>
+                    <div className="w-1/14 flex ml-12 flex-col items-center justify-center">
+                        <div
+                            className="bg-lime-500 rounded-full h-13 w-13 mb-2 text-white flex justify-center items-center">p
+                        </div>
+                        <p className="text-xs/6 w-9/12">تناسب اندام</p>
+                    </div>
+                    <div className="w-1/14 flex ml-12 flex-col items-center justify-center">
+                        <div
+                            className="bg-emerald-500 rounded-full h-13 w-13 mb-2 text-white flex justify-center items-center">p
+                        </div>
+                        <p className="text-xs/6 w-9/12">درکنار کوچولوها</p>
+                    </div>
+                    <div className="w-1/14 flex ml-12 flex-col items-center justify-center">
+                        <div
+                            className="bg-blue-800 rounded-full h-13 w-13 mb-2 text-white flex justify-center items-center">p
+                        </div>
+                        <p className="text-xs/6 w-9/12">خرید قسطی</p>
+                    </div>
+                    <div className="w-1/14 flex ml-12 flex-col items-center justify-center">
+                        <div
+                            className="bg-green-800 rounded-full h-13 w-13 mb-2 text-white flex justify-center items-center">p
+                        </div>
+                        <p className="text-xs/6 w-9/12">تخفیف طلای‌فیزیکی</p>
+                    </div>
+                    <div className="w-1/14 flex ml-12 flex-col items-center justify-center">
+                        <div
+                            className="bg-red-600 rounded-full h-13 w-13 mb-2 text-white flex justify-center items-center">p
+                        </div>
+                        <p className="text-xs/5  w-full">دوشنبه‌های به‌صرفه فروشگاه‌حضوری</p>
+                    </div>
+                    <div className="w-1/14 flex flex-col items-center justify-center">
+                        <div
+                            className="bg-gray-200 text-gray-400 rounded-full h-13 w-13 mb-2  flex justify-center items-center font-[1000]">...
+                        </div>
+                        <p className="text-xs/1 w-full">بیشتر</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default ServiceCategories;
---
+++ app/[seggestion]/superSuggest.js
@@ -0,0 +1,21 @@
+const SuperSuggest = () => {
+    return (
+        <>
+        <div className="w-full h-[20rem] flex items-center justify-center mt-3">
+            <div className="w-10/12 h-full bg-red-600 rounded-2xl py-5 flex justify-center">
+                <div className="h-full flex flex-col items-center justify-between w-1/9">
+                    <p className="text-white w-full text-center text-3xl/7">پیشنهاد شگفت انگیز</p>
+                    <p className="text-white w-full text-center"><span className="bg-white text-black rounded-lg">10</span> : 10 : 10</p>
+                    <p className="text-white w-15 text-center text-7xl">%</p>
+                    <p className="text-white w-full text-center">  مشاهده همه » </p>
+                </div>
+                <div className="bg-white w-8/9 rounded-r-2xl" >
+                    ...
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default SuperSuggest;
