e484a03dbcdec53e3018ef4edb8e19f2331bfae0
Hossein Nekouie <hossein373737@gmail.com>
2025-10-15 22:31:04 +0330

    debug

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client"
 import Header from "@/app/components/layout/header";
 import Footer from "@/app/components/layout/footer";
-import MainPage from "@/app/(pages)/page";
+import MainPage from "@/app/(main-content)/page";
 import FixedItem from "@/app/components/fixed/fixedItem";
 import {Provider} from "react-redux";
 
